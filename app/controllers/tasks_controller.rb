ec65f6b7d22db0a63d924e9a0fd393fd5ef3e724
Bali Tok <bali.tok@hotmail.com>
2023-03-07 12:45:22 +0800

    Added post route to create a new task, added links to go back a forth between tasks and tasks/new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,15 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.create!(task_params)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
   get '/tasks/:id', to: 'tasks#show'
+  post 'tasks', to: 'tasks#create'
 end
